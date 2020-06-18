f0000350d37b42470e2d5b5f7c1d418d263d3d6a
HanSolo23 <vanotaran@gmail.com>
2020-06-18 16:58:38 +0300

    start working with bot

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask_sslify import SSLify
+import requests
 
-app = Flask(__name__)
+#app = Flask(__name__)
+#sslify = SSLify(app)
+#@app.route('/')
+#def index():
+    #return '<h1>Hi</h1>'
 
-@app.route('/')
-def index():
-    return '<h1>Hi</h1>'
+URL = 'https://api.telegram.org/bot1050128789:AAGO5PeFQ8DNdpTHLf_p37_aQWy505JhIF8/'
+
+def main():
+    r = requests.get(URL + 'getMe')
+    print(r.json())
+
 
 if __name__ == '__main__':
-    app.run()
+    main()
